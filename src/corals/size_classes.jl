02da597f945fa15c3e7adfbfba3327b9bb6e19e6
Takuya Iwanaga <takuyai@gmail.com>
2024-11-17 22:33:24 +1100

    Default to Float32

--- src/corals/size_classes.jl
+++ src/corals/size_classes.jl
@@ -1,11 +1,11 @@
 """
-    bin_edges()
+    bin_edges()::Matrix{Float32}
 
 Helper function defining coral colony diameter bin edges. The values are converted from `cm`
 to the desired unit. The default unit is `m`.
 """
-function bin_edges()
-    return Matrix(
+function bin_edges()::Matrix{Float32}
+    return Matrix{Float32}(
         [
             2.5 5.0 7.5 10.0 20.0 40.0 100.0 150.0;
             2.5 5.0 7.5 10.0 20.0 35.0 50.0 100.0;
@@ -16,10 +16,19 @@ function bin_edges()
     )
 end
 
-function diameter_size_classes()
+"""
+    diameter_size_classes()::Vector{Matrix{Float32}}
+
+Determine diameter widths for each size class.
+
+See also:
+- `bin_edges()`
+- `bin_widths()`
+"""
+function diameter_size_classes()::Vector{Matrix{Float32}}
     edges = bin_edges()
     n_groups = size(edges, 1)
-    edges = hcat(zeros(n_groups), edges)
+    edges = hcat(zeros(Float32, n_groups), edges)
 
     # TODO: Use YAXArrays (groups ⋅ class start ⋅ class end)
     return map(x -> [x[1:end-1] x[2:end]], eachrow(edges))
@@ -35,16 +44,16 @@ function bin_widths()
 end
 
 """
-    linear_extensions()
+    linear_extensions()::Matrix{Float32}
 
 Linear extensions.
 """
-function linear_extensions()::Matrix{Float64}
+function linear_extensions()::Matrix{Float32}
     return [
-        0.609456 1.07184 2.55149 5.07988 9.45091 16.8505 0.0;
-        0.768556 1.22085 1.86447 2.82297 3.52938 3.00422 0.0;
-        0.190455 0.343747 0.615467 0.97477 1.70079 2.91729 0.0;
-        0.318034 0.47385 0.683729 0.710587 0.581085 0.581085 0.0;
-        0.122478 0.217702 0.382098 0.718781 1.24172 2.08546 0.0
+        0.609456f0 1.07184f0 2.55149f0 5.07988f0 9.45091f0 16.8505f0 0.0f0;
+        0.768556f0 1.22085f0 1.86447f0 2.82297f0 3.52938f0 3.00422f0 0.0f0;
+        0.190455f0 0.343747f0 0.615467f0 0.97477f0 1.70079f0 2.91729f0 0.0f0;
+        0.318034f0 0.47385f0 0.683729f0 0.710587f0 0.581085f0 0.581085f0 0.0f0;
+        0.122478f0 0.217702f0 0.382098f0 0.718781f0 1.24172f0 2.08546f0 0.0f0
     ]
 end
