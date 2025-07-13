bb800cd0ce78971b81ab8cafc44eabf7582bf0c0
Takuya Iwanaga <takuyai@gmail.com>
2025-07-13 11:21:51 +1000

    Add more performance metrics

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,2 +1,8 @@
-RMSE(y_hat, y) = sqrt(mean((y_hat .- y).^2))
-R2(y_hat, y) = 1 - (sum((y .- y_hat).^2) / sum((y .- mean(y)).^2))
+RMSE(y_hat, y) = sqrt(mean((y_hat .- y) .^ 2))
+R2(y_hat, y) = 1 - (sum((y .- y_hat) .^ 2) / sum((y .- mean(y)) .^ 2))
+
+pearson(y_hat, y) = StatsBase.cor(y_hat, y)
+spearman(y_hat, y) = StatsBase.corspearman(y_hat, y)
+kendall(y_hat, y) = StatsBase.corkendall(y_hat, y)
+
+const ALL_METRICS = [RMSE, R2, pearson, spearman, kendall]
