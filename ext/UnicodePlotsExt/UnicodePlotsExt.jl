1a574a2f745dd07a56da654a3e623757413f443e
Takuya Iwanaga <takuyai@gmail.com>
2025-07-13 11:23:18 +1000

    Make plotting more generic

--- ext/UnicodePlotsExt/UnicodePlotsExt.jl
+++ ext/UnicodePlotsExt/UnicodePlotsExt.jl
@@ -12,43 +12,52 @@ function CoralFlow.viz.survival_performance_plots(
     model_fits::CoralFlow.PolySurvivalModel;
     target_groups::Vector{String}=CoralFlow.TARGET_GROUPS
 )
-    logclass = CoralFlow.LOGCLASS_ID
+    bin_id_col = CoralFlow.BIN_ID
 
     for group_id in 1:length(target_groups)
         group_df = groupings[target_groups[group_id]]
         max_group_size = maximum(group_df.diam)
 
-        n_bins = length(unique(group_df[!, logclass]))
+        bin_ids = unique(group_df[!, bin_id_col])
 
         # Training data plot
-        mean_train = [maximum(group_df[group_df[!, logclass].==i, CoralFlow.TRAIN_CLASS_MEAN_ID]) for i in 1:n_bins]
-        std_train = [maximum(group_df[group_df[!, logclass].==i, CoralFlow.TRAIN_CLASS_STD_ID]) for i in 1:n_bins]
+        mean_train = [maximum(group_df[group_df[!, bin_id_col].==i, CoralFlow.TRAIN_CLASS_MEAN_ID]) for i in bin_ids]
+        std_train = [maximum(group_df[group_df[!, bin_id_col].==i, CoralFlow.TRAIN_CLASS_STD_ID]) for i in bin_ids]
 
-        rmse = round(model_fits.performance.train.rmse[group_id]; digits=3)
-        r2 = round(model_fits.performance.train.r2[group_id]; digits=3)
-        train_title = "$(target_groups[group_id])\nTraining Data\nRMSE: $(rmse) | R²: $(r2)"
+        score_text = build_metric_display(model_fits.performance.train, group_id)
+        train_title = "$(target_groups[group_id])\nTraining Data\n$(score_text)"
 
         plt_train = scatterplot(mean_train; ylim=(0, 1.1), title=train_title)
         lineplot!(plt_train, min.(mean_train .+ std_train, 1.0))
         lineplot!(plt_train, mean_train .- std_train)
         lineplot!(plt_train, [model_fits[group_id](Float64(i)) for i in 0:max_group_size])
-        xlabel!(plt_train, "Diameter [cm]")
+        xlabel!(plt_train, "Diameter Bin")
         ylabel!(plt_train, "Survival")
 
         # Test data plot
-        mean_test = [maximum(group_df[group_df[!, logclass].==i, CoralFlow.TEST_CLASS_MEAN_ID]) for i in 1:n_bins]
-        std_test = [maximum(group_df[group_df[!, logclass].==i, CoralFlow.TEST_CLASS_STD_ID]) for i in 1:n_bins]
+        mean_test = [maximum(group_df[group_df[!, bin_id_col].==i, CoralFlow.TEST_CLASS_MEAN_ID]) for i in bin_ids]
+        std_test = [maximum(group_df[group_df[!, bin_id_col].==i, CoralFlow.TEST_CLASS_STD_ID]) for i in bin_ids]
 
-        rmse = round(model_fits.performance.test.rmse[group_id]; digits=3)
-        r2 = round(model_fits.performance.test.r2[group_id]; digits=3)
-        test_title = "$(target_groups[group_id])\nTest Data\nRMSE: $(rmse) | R²: $(r2)"
-        plt_test = scatterplot(mean_test; ylim=(0, 1.1), title=test_title, name="Training data")
+        score_text = build_metric_display(model_fits.performance.test, group_id)
+        test_title = "$(target_groups[group_id])\nTest Data\n$(score_text)"
+
+        plt_test = scatterplot(mean_test; ylim=(0, 1.1), title=test_title, name="Observed")
         lineplot!(plt_test, min.(mean_test .+ std_test, 1.0), name="+1 stdev")
         lineplot!(plt_test, mean_test .- std_test, name="-1 stdev")
         lineplot!(plt_test, [model_fits[group_id](Float64(i)) for i in 0:max_group_size], name="Model")
-        xlabel!(plt_test, "Diameter [cm]")
+        xlabel!(plt_test, "Diameter Bin")
         display(UnicodePlots.panel(plt_train) * UnicodePlots.panel(plt_test))
     end
+end
+
+function build_metric_display(results::NamedTuple, idx::Int64)
+    res_text = []
+    for m in CoralFlow.ALL_METRICS
+        _s = getfield(results, Symbol(m))[idx]
+        push!(res_text, "$(string(m)): $(round(_s; digits=3))")
+    end
+
+    return join(res_text, " | ")
 end
 
 """
@@ -68,9 +77,8 @@ function CoralFlow.viz.growth_performance_plots(
         xi = sub_df.diam[x_idx]
         yi = sub_df.diamnext[x_idx]
 
-        rmse = round(model_fits.performance.train.rmse[group_id]; digits=3)
-        r2 = round(model_fits.performance.train.r2[group_id]; digits=3)
-        train_title = "$(target_groups[group_id])\nTraining Data\nRMSE: $(rmse) | R²: $(r2)"
+        score_text = build_metric_display(model_fits.performance.train, group_id)
+        train_title = "$(target_groups[group_id])\nTraining Data\n$(score_text)"
 
         # Main.@infiltrate
         model = model_fits[group_id]
@@ -85,10 +93,9 @@ function CoralFlow.viz.growth_performance_plots(
         xi = sub_df.diam[x_idx]
         yi = sub_df.diamnext[x_idx]
 
-        rmse = round(model_fits.performance.test.rmse[group_id]; digits=3)
-        r2 = round(model_fits.performance.test.r2[group_id]; digits=3)
-        test_title = "$(target_groups[group_id])\nTest Data\nRMSE: $(rmse) | R²: $(r2)"
-        plt_test = scatterplot(yi; title=test_title, name="Training data")
+        score_text = build_metric_display(model_fits.performance.test, group_id)
+        test_title = "$(target_groups[group_id])\nTest Data\n$(score_text)"
+        plt_test = scatterplot(yi; title=test_title, name="Observed")
         lineplot!(plt_test, [model(Float64(i)) for i in xi], name="Model")
         xlabel!(plt_test, "Diameter [cm]")
         display(UnicodePlots.panel(plt_train) * UnicodePlots.panel(plt_test))
