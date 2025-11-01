8a326b591a8d1e0fa2705ec3f6f54c34cbc71f85
Takuya Iwanaga <takuyai@gmail.com>
2025-11-01 14:56:44 +1100

    auto-format

--- src/interface/regressions.jl
+++ src/interface/regressions.jl
@@ -1,16 +1,20 @@
-function fit_growth_models(groupings::OrderedDict{String,DataFrame}; degree::Int=2)::PolyGrowthModel
+function fit_growth_models(
+    groupings::OrderedDict{String,DataFrame}; degree::Int=2
+)::PolyGrowthModel
     models = PolyGrowthFunction[]
 
     g = length(values(groupings))
     m = length(ALL_METRICS)
 
-    train_ = NamedTuple((Symbol(k) => Vector{Float32}(undef, g) for k in CoralFlow.ALL_METRICS))
+    train_ = NamedTuple((
+        Symbol(k) => Vector{Float32}(undef, g) for k in CoralFlow.ALL_METRICS
+    ))
     test_ = deepcopy(train_)
 
     for (i, (_, df)) in enumerate(groupings)
         isempty(df) && continue
 
-        sub_df = df[df[!, TRAIN_CLASS].>0, :]
+        sub_df = df[df[!, TRAIN_CLASS] .> 0, :]
 
         x_idx = sortperm(sub_df.diam)
         xi = sub_df.diam[x_idx]
@@ -30,7 +34,7 @@ function fit_growth_models(groupings::OrderedDict{String,DataFrame}; degree::Int
         end
 
         # As above, but for test data
-        sub_df = df[df[!, TEST_CLASS].>0, :]
+        sub_df = df[df[!, TEST_CLASS] .> 0, :]
         x_idx = sortperm(sub_df.diam)
         xi = sub_df.diam[x_idx]
         yi = sub_df.diamnext[x_idx]
@@ -48,7 +52,7 @@ function fit_growth_models(groupings::OrderedDict{String,DataFrame}; degree::Int
         models,
         (;
             train=train_,
-            test=test_,
+            test=test_
         )
     )
 end
@@ -63,18 +67,22 @@ Fit survival models to grouped coral data using logistic regression.
 # Returns
 Tuple of (models, mcfadden_r2_scores, log_likelihood_scores, brier_scores)
 """
-function fit_survival_models(groupings::OrderedDict{String,DataFrame}; degree::Int64=2)::PolySurvivalModel
+function fit_survival_models(
+    groupings::OrderedDict{String,DataFrame}; degree::Int64=2
+)::PolySurvivalModel
     models = PolySurvivalFunction[]
     g = length(values(groupings))
     m = length(ALL_METRICS)
 
-    train_ = NamedTuple((Symbol(k) => Vector{Float32}(undef, g) for k in CoralFlow.ALL_METRICS))
+    train_ = NamedTuple((
+        Symbol(k) => Vector{Float32}(undef, g) for k in CoralFlow.ALL_METRICS
+    ))
     test_ = deepcopy(train_)
 
     for (i, (_, df)) in enumerate(groupings)
         isempty(df) && continue
 
-        sub_df = df[df.class_train.>0, :]
+        sub_df = df[df[!, TRAIN_CLASS] .> 0, :]
 
         # Fit model based on observed size at time of mortality (`diamnext`)
         x_idx = sortperm(sub_df.diamnext)
@@ -97,7 +105,7 @@ function fit_survival_models(groupings::OrderedDict{String,DataFrame}; degree::I
         end
 
         # Repeat above for test data
-        sub_df = df[df.class_test.>0, :]
+        sub_df = df[df.class_test .> 0, :]
         x_idx = sortperm(sub_df.diamnext)
         xi = sub_df.diamnext[x_idx]
         yi = sub_df.class_test_mean[x_idx]
@@ -114,7 +122,7 @@ function fit_survival_models(groupings::OrderedDict{String,DataFrame}; degree::I
         models,
         (;
             train=train_,
-            test=test_,
+            test=test_
         )
     )
 end
